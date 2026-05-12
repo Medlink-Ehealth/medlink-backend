e813b74fc5bc7a24a89a2a524d7ccbf81e25284b
DevAkintunde <devakintunde@gmail.com>
2026-05-12 21:26:37 +0100

    Did some housekeeping. Moved to deploying directly to dev-sync branch on org repo, and improved README.md readability.

--- apps/auth-service/redis.config.ts
+++ apps/auth-service/redis.config.ts
@@ -2,10 +2,8 @@ import "dotenv/config";
 import { logger } from "@medlink/common";
 const envs = process.env;
 
-const forceRedisInit = envs.MEMORY_CACHE?.toLowerCase() === "redis" || envs.CONVERSATION_STORE?.toLowerCase() === "redis"; // where a engine/procss exists that relies on redis, force an initializing even if envs.REDIS_CLIENT is unset - using local connection if available
-
 export const redisConfig = (): { port: number; host: string } | null => {
-	if (envs.REDIS_CLIENT || forceRedisInit)
+	if (envs.REDIS_CLIENT)
 		try {
 			const redisInfo = envs.REDIS_CLIENT && (JSON.parse(envs.REDIS_CLIENT) as [string, number]);
 			if (redisInfo && Array.isArray(redisInfo)) {
@@ -13,13 +11,7 @@ export const redisConfig = (): { port: number; host: string } | null => {
 					port: redisInfo[1] && typeof redisInfo[1] === "number" ? redisInfo[1] : 6379, // Redis port
 					host: redisInfo[0] && typeof redisInfo[0] === "string" ? redisInfo[0] : "127.0.0.1", // Redis host
 				};
-			} else if (forceRedisInit)
-				// use default
-				return {
-					port: 6379,
-					host: "127.0.0.1",
-				};
-			else return null;
+			} else return null;
 		} catch (err) {
 			logger.error("Unable to parse redis configuration in env: ", err);
 		}
